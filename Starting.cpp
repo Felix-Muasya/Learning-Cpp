0be42036385b73bd6e485074fc1597676b6485d5
felix-muasya <66355304+Felix-Muasya@users.noreply.github.com>
2022-10-17 13:13:42 +0300

    Add files via upload

---
+++ Starting.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    std::cout << "I Like games \n";
+    std::cout <<"1st C++ code compiled successfully" <<std::endl;
+    std::cout <<"Done for now at least";
+    return 0;
+}
+
+
+
