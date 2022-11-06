8e93354faeffd82c1377d6d35d51e6f8ca86777e
felix-muasya <66355304+Felix-Muasya@users.noreply.github.com>
2022-11-06 11:20:33 +0300

    Add files via upload

---
+++ Variables.cpp
@@ -0,0 +1,45 @@
+# include <iostream>
+
+int main(){
+
+
+    int x = 5; // declaration
+    int y = 6;
+    int sum = x+y;
+
+    std::cout << x << '\n';
+    std::cout << y << '\n';
+    std::cout << sum << '\n';
+
+
+    // integers, int can only store whole numbers
+    int age = 51;
+    int year = 2022;
+    int days = 7.5;
+
+    // double, numbers including decimals
+    double temp = 22.56;
+
+    // single characters
+    char grade = 'A';
+    char initial = 'C';
+
+    // boolean, true or false
+    bool student = false;
+    bool game = true;
+
+    std :: cout << game << '\n';
+    std :: cout << student << '\n';
+
+    // string, objects that represent a sequence of text
+
+    std::string name = "Muasya the 1st";
+    std::cout << "Felix " << name; //string literal
+
+
+
+
+
+
+    return 0;
+}
