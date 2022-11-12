9742e2c3842cb33d5c151a77682b561a05d4f2e2
felix-muasya <66355304+Felix-Muasya@users.noreply.github.com>
2022-11-12 11:03:13 +0300

    Add files via upload

---
+++ billioncounter.cpp
@@ -0,0 +1,9 @@
+# include <iostream>
+
+int main ()
+{
+    int n = 0;
+    while (n != 1000){
+    n++;
+    std::cout<<n<< '\n';}
+}
---
+++ simple clock.cpp
@@ -0,0 +1,47 @@
+/*
+This is simple C++ program that shows the time based on the user's
+input, There is an increment function that allows for time time to change
+( when 60 seconds/minutes pass 1 Minute is added to the time.
+ */
+# include <iostream>
+# include <windows.h>
+
+using namespace std;
+int main ()
+{
+    int h, m, s, a, err;
+    err=a=0;
+    while (err==0){
+        cout << "Enter Hour: "<< '\n';
+        cin >> h;
+        cout << "Enter Minutes: " << '\n';
+        cin >> m;
+        cout << "Enter Seconds: " << '\n';
+        cin >> s;
+        if (h <24&&m < 60&&s <60)
+        err++;
+        else
+        system("cls");}
+    while (a==0)
+    {
+        system("cls");
+        cout << h<< ":"<< m<< ":"<<s<<endl;
+        Sleep(1000);
+        s++;
+        if (s>59) // checking whether the seconds being input are greater than 59
+            {
+            s=0;
+            m++; // simple increment, adding 1 minute to our time
+            }
+        if (m>59) // checking whether the minutes being input are greater than 59
+            {
+            m = 0;
+            h++; // simple increment, adding 1 hour to the time
+            }
+        if (h>24) // checking whether the hours being input are greater than 24
+            {
+            h=0; // simple increment, starting a new day
+            }
+    }
+    return 0;
+}
